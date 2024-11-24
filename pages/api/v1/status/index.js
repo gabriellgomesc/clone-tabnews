d4f3cbe4dcf8281c9f7cb3f0ba2659ff82c0a3a5
Gabriel Gomes <108940113+gabriellgomesc@users.noreply.github.com>
2024-11-24 19:06:27 +0000

    finish `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,10 +1,38 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
+  const updatedAt = new Date().toISOString();
 
-  response.status(200).json({ chave: "são acima da média" });
+  let databaseVersionValue = "";
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  databaseVersionValue = databaseVersionResult.rows[0].server_version;
+
+  const databaseMaxConnectionResult = await database.query(
+    "SHOW max_connections;",
+  );
+  const databaseMaxConnectionValue = parseInt(
+    databaseMaxConnectionResult.rows[0].max_connections,
+  );
+
+  const databaseName = process.env.POSTGRES_DB;
+
+  const databaseOpenedConnectionsResult = await database.query({
+    text: "SELECT count(*)::int AS opened_connections FROM pg_stat_activity WHERE datname = $1",
+    values: [databaseName],
+  });
+  const databaseOpenedConnectionsValue =
+    databaseOpenedConnectionsResult.rows[0].opened_connections;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+        max_connections: databaseMaxConnectionValue,
+        opened_connections: databaseOpenedConnectionsValue,
+      },
+    },
+  });
 }
 
 export default status;
